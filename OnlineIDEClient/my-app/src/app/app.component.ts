e90b6ae157cc95f5bd284b90fa01c4138e14f714
Ofek Ashkenazi <ofekash@post.bgu.ac.il>
2020-04-07 20:23:34 +0300

    7/4/2020

--- OnlineIDEClient/my-app/src/app/app.component.ts
+++ OnlineIDEClient/my-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, AfterViewInit} from '@angular/core';
 import {CodeEditorComponent} from './codeEditor/codeEditor.component';
 import {Program} from '../BL/Program';
+import {SharedService} from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,10 @@ export class AppComponent implements AfterViewInit {
   // private staticDebbuger = CodeEditorComponent.debugger;
   private program = new Program();
 
+  private sharedService = new SharedService();
 
   ngAfterViewInit() {
-    this.program.subscribeOutputStream(CodeEditorComponent);
+    this.program.subscribeOutputStream(this.sharedService);
   }
 
 }
