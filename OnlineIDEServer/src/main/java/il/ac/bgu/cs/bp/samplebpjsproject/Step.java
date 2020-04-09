9dad45fe04153cf79cc6b0b7628100ba7cbb76e1
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2020-04-09 15:10:30 +0300

    more continuation updates

--- OnlineIDEServer/src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/Step.java
+++ OnlineIDEServer/src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/Step.java
@@ -1,22 +1,21 @@
 package il.ac.bgu.cs.bp.samplebpjsproject;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.Field;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.stream.Collectors;
 
 import il.ac.bgu.cs.bp.bpjs.bprogramio.BProgramSyncSnapshotCloner;
 import il.ac.bgu.cs.bp.bpjs.bprogramio.BProgramSyncSnapshotIO;
+import il.ac.bgu.cs.bp.bpjs.internal.Pair;
 import il.ac.bgu.cs.bp.bpjs.model.BEvent;
 import il.ac.bgu.cs.bp.bpjs.model.BProgram;
 import il.ac.bgu.cs.bp.bpjs.model.BProgramSyncSnapshot;
 import il.ac.bgu.cs.bp.bpjs.model.BThreadSyncSnapshot;
 import il.ac.bgu.cs.bp.bpjs.model.FailedAssertion;
 import il.ac.bgu.cs.bp.bpjs.model.eventsets.EventSet;
+import org.mozilla.javascript.NativeContinuation;
 
 public class Step {
 	private final BProgramSyncSnapshot bpss;
@@ -65,15 +64,37 @@ public class Step {
 		List<EventSet> wait = bpss.getStatements().stream().map(s->s.getWaitFor()).collect(Collectors.toList());
 		List<EventSet> blocked = bpss.getStatements().stream().map(s->s.getBlock()).collect(Collectors.toList());
 		List<BEvent> requested = bpss.getStatements().stream().map(s->s.getRequest()).flatMap(l->l.stream()).collect(Collectors.toList());
-		bpss.getBThreadSnapshots().forEach(s->System.out.println(s.getContinuationProgramState().toString()));
-//		int[] rows = threadSnapshots.stream().map(s->Map.copyOf(s.getName(),s.getContinuationProgramState().getFrameIndex()));
+
+		// the following map structure is:
+		/*
+		Key = bthread name
+		Value.Left = Current row
+		Value.Right = Map of stack variables, with the following structure: Key = variable name, Value = variable value (as json string)
+		 */
+		Map<String, Pair<Integer, Map<String, String>>> bThreadDebugData = new HashMap<>();
+		bpss.getBThreadSnapshots().forEach(s-> {
+			// to find the stack variables and the current line, you need to dig in the continuation object
+			// to see how to work with this object, take a look at https://github.com/bThink-BGU/BPjs/blob/develop/src/main/java/il/ac/bgu/cs/bp/bpjs/model/internal/ContinuationProgramState.java
+			Object continuation = ((NativeContinuation)s.getContinuation()).getImplementation();
+			int lineNumber = -1;
+			Map<String, String> variables = new HashMap<>();
+			bThreadDebugData.put(s.getName(), new Pair<>(lineNumber, variables));
+		});
+
 		return new StepMessage(
 				new BProgramSyncSnapshotIO(bprog).serialize(bpss),
+				bThreadDebugData,
 				null,
 				requested.stream().map(r->r.toString()).collect(Collectors.toList()),
 				selectableEvents.stream().map(e->e.toString()).collect(Collectors.toList()),
 				wait.stream().map(r->r.toString()).collect(Collectors.toList()),
 				blocked.stream().map(r->r.toString()).collect(Collectors.toList()),
 				selectedEvent.toString());
+	}
+
+	private Object getValue( Object instance, String fieldName ) throws NoSuchFieldException, IllegalAccessException {
+		Field fld = instance.getClass().getDeclaredField(fieldName);
+		fld.setAccessible(true);
+		return fld.get(instance);
 	}
 }
--- OnlineIDEServer/src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/StepMessage.java
+++ OnlineIDEServer/src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/StepMessage.java
@@ -1,12 +1,15 @@
 package il.ac.bgu.cs.bp.samplebpjsproject;
 
+import il.ac.bgu.cs.bp.bpjs.internal.Pair;
+
 import java.util.List;
 import java.util.Map;
 
 public class StepMessage {
 	public final String type;
 	public final byte[] bpss;
-	public final Map<String,Object> bpStack;
+	public final Map<String, Pair<Integer, Map<String, String>>> bThreadDebugData;
+	public final Map<String,String> globalVariables;
 	public final List<String> reqList;
 	public final List<String> selectableEvents;
 	public final List<String> waitList;
@@ -14,11 +17,13 @@ public class StepMessage {
 	public final String selectedEvent;
 	
 	
-	public StepMessage(byte[] bpss, Map<String, Object> bpStack, List<String> reqList, List<String> selectableEvents, List<String> wait,
+	public StepMessage(byte[] bpss, Map<String, Pair<Integer, Map<String, String>>> bThreadDebugData, Map<String, String> globalVariables,
+					   List<String> reqList, List<String> selectableEvents, List<String> wait,
 					   List<String> block, String selectedEvent) {
 		this.type = "step";
 		this.bpss = bpss;
-		this.bpStack = bpStack;
+		this.bThreadDebugData = bThreadDebugData;
+		this.globalVariables = globalVariables;
 		this.reqList = reqList;
 		this.selectableEvents = selectableEvents;
 		this.waitList = wait;
